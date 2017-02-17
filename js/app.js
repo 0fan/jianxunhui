b7678afc5c4248dbff62d3bf18fca895d7127893
linfan <dear_moo@qq.com>
2017-02-17 16:39:10 +0800

    修改活动列表页和活动详情页，我发现应该把页面的代码写在每个页面的pageInit事件里，不然用框架的路由跳转会发生页面代码不执行的情况

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 (function(){
 
+$(document).on("pageInit", function(e, pageId, $page) {
+  if(pageId == "page-activityDetail") {
+  	// flash
+  	var flash = new Swiper('#flash',{
+  		pagination : '.swiper-pagination'
+  	});
+  }
+  if(pageId == "page-activityList") {
+  	
+  }
+});
+
 $.init();
 
 })();
